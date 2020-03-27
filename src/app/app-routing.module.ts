7dc6abce5c6c15ece9f091b7c0013ab1ec6b45b2
Hassan Tayyab <hassantayyab@devstudio.us>
2020-03-28 04:56:55 +0500

    folder structure setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,35 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
+
+import { AuthGuard } from './shared/guards/auth.guard';
+import { NoAuthGuard } from './shared/guards/no-auth.guard';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'authentication',
+    loadChildren: () => import('./modules/pages/authentication/authentication.module').then(m => m.AuthenticationModule),
+    canLoad: [NoAuthGuard]
+  },
+  {
+    path: 'dashboard',
+    loadChildren: () => import('./modules/pages/dashboard/dashboard.module').then(m => m.DashboardModule),
+    canLoad: [AuthGuard]
+  },
+  {
+    path: 'settings',
+    loadChildren: () => import('./modules/pages/settings/settings.module').then(m => m.SettingsModule),
+    canLoad: [AuthGuard]
+  },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  {
+    path: '**',
+    loadChildren: () => import('./modules/pages/page-not-found/page-not-found.module').then(m => m.PageNotFoundModule)
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
